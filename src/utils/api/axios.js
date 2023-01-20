4690934c6680f716ce0ba4903f4fbc9823ff40c5
ryumartinez <ryueduardomartinez@outlook.es>
2023-01-20 15:58:41 -0300

    nuevo link de backend

--- src/utils/api/axios.js
+++ src/utils/api/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const client = axios.create({ baseURL: "http://localhost:3500" });
+const client = axios.create({ baseURL: "https://houseofdevback.onrender.com" });
 
 const tokenJSON = window.localStorage.getItem("token");
 
